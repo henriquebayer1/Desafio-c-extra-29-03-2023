fdb89abed0a755ba5a072a8086343298ae893780
Henrique Jordão Baier De Azevedo <45922978810@info.local>
2023-03-30 07:55:16 -0300

    desafio extra

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine($"Digite o seu ano de nascimento:");
+int anoDeNascimento = int.Parse(Console.ReadLine());
+
+float idadeAnos = (2023 - anoDeNascimento);
+
+float idadeSemanas = (idadeAnos * 52);
+
+Console.WriteLine($"a sua idade em semanas e de: {idadeSemanas}");
+
